3c10147694bc7e2e81f15266d8b9c1e8dcf4436a
888-coding <pachelee@gmail.com>
2023-10-08 07:02:58 -0300

    Criando excel correto

--- criarexcel.py
+++ criarexcel.py
@@ -1,2 +1,10 @@
 # Bibliotecas 
 import pandas as pd 
+d = {
+       'Nome': ["Pablo", 'Thais'],
+       'Idade':[43, 40],
+       'Altura': [180, 170]       
+}
+dados = pd.DataFrame(d)
+print(dados)
+dados.to_excel("teste.xlsx", index = False)
